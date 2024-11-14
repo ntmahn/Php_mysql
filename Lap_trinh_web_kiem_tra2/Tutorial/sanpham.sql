ba2b74191f83af6fb8fa082ce91045ceb26b6ded
TUANNN <tuannnm510@gmail.com>
2024-11-14 12:28:20 +0700

    csdl moi

--- Lap_trinh_web_kiem_tra2/Tutorial/sanpham.sql
+++ Lap_trinh_web_kiem_tra2/Tutorial/sanpham.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 01, 2024 lúc 02:24 PM
--- Phiên bản máy phục vụ: 10.4.32-MariaDB
--- Phiên bản PHP: 8.0.30
+-- Thời gian đã tạo: Th10 14, 2024 lúc 05:13 AM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `qlbanhang`
+-- Cơ sở dữ liệu: `ktra2`
 --
 
 -- --------------------------------------------------------
@@ -31,6 +31,7 @@ CREATE TABLE `sanpham` (
   `mahang` varchar(5) NOT NULL,
   `tenhang` varchar(30) NOT NULL,
   `giahang` decimal(10,0) NOT NULL,
+  `diachi` varchar(100) NOT NULL,
   `hinhanh` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -38,18 +39,13 @@ CREATE TABLE `sanpham` (
 -- Đang đổ dữ liệu cho bảng `sanpham`
 --
 
-INSERT INTO `sanpham` (`mahang`, `tenhang`, `giahang`, `hinhanh`) VALUES
-('SP01', 'Bot Giat OMO', 200000, 'botgiatomo.jpg'),
-('SP03', 'Bot giat Sufff', 250000, 'botgiatsuf.jpg'),
-('SP111', 'sdfsafdsafsa', 0, 'bai-3.png'),
-('SP112', 'fdfgdsgfds', 0, 'anh7.jpg'),
-('SP114', 'San pham tot', 0, 'anh7.jpg'),
-('SP115', 'San pham chua tot', 0, 'anh7.jpg'),
-('SP15', 'San pham kem danh rang', 0, 'anh1.jpg'),
-('SP2', 'Sua rua mat tri mun', 1500000, 'botgiatlix.jpg'),
-('SP20', 'kem danh rang', 0, 'anh4.jpg'),
-('SP4', 'Sua tam LIX', 1500000, 'botgiatlix.jpg'),
-('SP40', 'May do hoi nuoc', 0, 'anh4.jpg');
+INSERT INTO `sanpham` (`mahang`, `tenhang`, `giahang`, `diachi`, `hinhanh`) VALUES
+('DA001', 'Chung cư tòa C Westbay', 1, 'chung cư tòa C Westbay hướng Bắc, Ecopark Văn Giang', 'DA001.jpg'),
+('DA002', 'chung cư Sunshine Riverside Tâ', 1, 'Sunshine Riverside, Phường Phú Thượng, Tây Hồ, Hà Nội', 'DA002.jfif'),
+('DA003', 'Chung cư GoldView', 1, '346 Bến Vân Đồn, Quận 4, TP. Hồ Chí Minh', 'DA003.jfif'),
+('DA004', 'Vinhomes Central Park', 1, '208 Nguyễn Hữu Cảnh, Quận Bình Thạnh, TP. Hồ Chí Minh', 'DA004.jfif'),
+('DA005', 'The Sun Avenue', 1, '28 Mai Chí Thọ, An Phú, Quận 2, TP. Hồ Chí Minh', 'DA005.jfif'),
+('DA006', 'Times City', 1, '458 Minh Khai, Hai Bà Trưng, Hà Nội', 'DA006.jfif');
 
 --
 -- Chỉ mục cho các bảng đã đổ
